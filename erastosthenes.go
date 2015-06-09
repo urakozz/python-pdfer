6fbf93a77b7e8ed814e11cb8d2ad5852b0d25cb4
Yury Kozyrev <urakozz@me.com>
2015-06-09 15:21:18 +0200

    Create erastosthenes.go

---
+++ erastosthenes.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Erastosthenes(n int64) ([]int64) {
+	shleve := make([]bool, n+1)
+
+	shleve[0] = true
+	shleve[1] = true
+	var primes []int64
+	for i := int64(2); i <= n; i++ {
+		if (!shleve[i]) {
+			primes = append(primes, i)
+			for j := int64(i); i * j < n + 1; j++ {
+				shleve[i*j] = true
+			}
+		}
+	}
+	fmt.Println(len(primes))
+
+
+
+	return primes
+}
